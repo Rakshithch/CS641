434907d4aba220fce0927fbf2aa712b478201ca4
Rakshithch <85780957+Rakshithch@users.noreply.github.com>
2024-10-10 20:12:52 -0400

    Component

--- week2clsslab/App.tsx
+++ week2clsslab/App.tsx
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ActivityIndicator, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, ScrollView, Button } from 'react-native';
+
+const CounterComponent = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount);
+
+  return (
+    <View style={styles.component}>
+      <Text>Count: {count}</Text>
+      <Button title="Increase Count" onPress={() => setCount(count + 1)} />
+    </View>
+  );
+};
+
+const TextComponent = ({ initialText }) => {
+  const [text, setText] = useState(initialText);
+
+  return (
+    <View style={styles.component}>
+      <Text>{text}</Text>
+      <Button title="Change Text" onPress={() => setText('Text Updated!')} />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text>Yay!!</Text>
       <Text>Week2 Class Lab is DONE!!</Text>
-      <ActivityIndicator></ActivityIndicator>
-      <Image source={{uri: "https://www.pace.edu/sites/default/files/styles/16_9_1600x900/public/2022-04/provost-interior-hero.jpg?h=993b43e0&itok=DUs3eLCi"}}
-      style={styles.tinyLogo} />
+      <ActivityIndicator />
+      <Image
+        source={{
+          uri: 'https://www.pace.edu/sites/default/files/styles/16_9_1600x900/public/2022-04/provost-interior-hero.jpg?h=993b43e0&itok=DUs3eLCi',
+        }}
+        style={styles.tinyLogo}
+      />
+
+      {}
+      <CounterComponent initialCount={0} />
+
+      {}
+      <TextComponent initialText="Initial Text" />
+
       <StatusBar style="auto" />
     </ScrollView>
   );
@@ -24,5 +58,9 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 500,
     height: 500,
+  },
+  component: {
+    marginVertical: 20,
+    alignItems: 'stretch',
   },
 });
